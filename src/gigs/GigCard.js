d5f2107b186aa5d1d6abbf62c5685dc1904a6fd6
James Reid <jreidmke@gmail.com>
2021-02-21 13:50:52 -0600

    completed platform details wf

---
+++ src/gigs/GigCard.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+function GigCard({gig}) {
+    return(
+        <div className="card">
+            <div className="card-text">
+                <Link to={`/gigs/${gig.id}`}>{gig.title}</Link>
+                <p>{gig.description.substring(0, 100)}...</p>
+            </div>
+        </div>
+    )
+};
+
+export default GigCard;
--- src/platforms/PlatformDetails.js
+++ src/platforms/PlatformDetails.js
@@ -2,16 +2,19 @@ import {useState, useEffect, useContext} from 'react';
 import PrintApi from '../api/api';
 import { useParams, Link } from "react-router-dom";
 import UserContext from '../auth/UserContext';
+import GigCard from '../gigs/GigCard';
 
 function PlatformDetails() {
     const { currentUser } = useContext(UserContext);
     const { platformId } = useParams();
     const [platform, setPlatform] = useState();
+    const [gigs, setGigs] = useState();
 
     useEffect(() => {
         async function getPlatform() {
             const platformRes = await PrintApi.getPlatformById(platformId);
             setPlatform(platformRes);
+            setGigs(platformRes.gigs);
         };
         getPlatform();
     }, []);
@@ -21,7 +24,8 @@ function PlatformDetails() {
         <div>
             <h1>Platform Details</h1>
             {platform ? <h1>{platform.displayName} {platform.description}</h1> : <h1>Loading</h1>}
-            
+            <h2>GIGS</h2>
+            {gigs ? gigs.map(g => <GigCard key={g.id} gig={g}/>) : ""}
             {currentUser.platformId == platformId ? "AUTH" : "NO AUTH"}
         </div>
     )
