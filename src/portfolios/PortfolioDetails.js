86c8208e6a66fd3d71677b61321e778678f2bdc1
James Reid <jreidmke@gmail.com>
2021-03-02 14:34:51 -0600

    completed portfolio details

--- src/portfolios/PortfolioDetails.js
+++ src/portfolios/PortfolioDetails.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import PrintApi from '../api/api';
 import UserContext from '../auth/UserContext';
 import { Link } from 'react-router-dom';
+import "./PortfolioDetails.css";
 
 function PortfolioDetails() {
     const { currentUser } = useContext(UserContext);
@@ -18,19 +19,47 @@ function PortfolioDetails() {
     }, []);
 
     return(
-        <div>
-            {portfolio ? <h1>{portfolio.title} </h1> : "Loading"}
+        <div className="container mt-2">
+            {portfolio ?
+            <div>
 
-            {}
+               <div className="row">
+                   <div className="col">
+                       <h1>{portfolio.title}</h1>
+                       <h5>
+                           A Portfolio By: {portfolio.firstName} {portfolio.lastName}
+                        </h5>
+                        {currentUser && portfolio && currentUser.writerId == portfolio.writerId ? <h5><Link to={`/portfolios/${portfolio.id}/edit`}>Edit or Add Pieces</Link></h5>: ""}
 
-            {portfolio ? portfolio.pieces.map(p => <p><Link to={`/pieces/${p.id}`}>{p.title}</Link> {p.text}</p>): "Pieces"}
+                   </div>
+               </div>
 
-            {portfolio ? portfolio.tags.map(t => <p>{t.title}</p>) : "Tags"}
+                <div className="row">
+                    <div className="col-4">
+                        <h5>Includes Pieces Tagged With:</h5>
+                        <ul id="tagList">
+                            {portfolio.tags.map(t => <li key={t.id}>{t.title}</li>)}
+                        </ul>
+                    </div>
 
-            {currentUser && portfolio && currentUser.writerId == portfolio.writerId ? <Link to={`/portfolios/${portfolio.id}/edit`}>Edit</Link> : "This portfolio doesn't belong to writer"}
+                    <div className="col">
+                        {portfolio.pieces.map(p => 
+                        <div key={p.id} className="overflow-auto" id="pieces">
+                            <h5><b>{p.title}</b></h5>
+                            <p>Submitted At: {p.createdAt.slice(0, 10)}</p>
+                            <p>{p.text}</p>
+                        </div>)
+                        }
+                    </div>
+                </div>
+
+            </div>
+
+            : ""}
 
         </div>
     )
 };
 
 export default PortfolioDetails;
+
